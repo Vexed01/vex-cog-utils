8a17fb164fd80f47eac359e62c7ad2ebf10718d3
Vexed01 <gh.vexed@gmail.com>
2021-04-08 14:25:07 +0100

    [0.0.0] ignore VSC

--- setup.py
+++ setup.py
@@ -3,11 +3,11 @@ from setuptools import setup
 setup(
     name="Vex-Cog-Utils",
     url="https://github.com/Vexed01/vex-cog-utils",
-    author="Vexed01"
+    author="Vexed01",
     author_email="gh.vexed@gmail.com",
-    description="Utility functions for the Vex-Cogs repo."
-    long_description=open("README.md").read()
-    long_description_content_type="text/markdown"
+    description="Utility functions for the Vex-Cogs repo.",
+    long_description=open("README.md").read(),
+    long_description_content_type="text/markdown",
     packages=["cogutils"],
     install_requires=["Red-DiscordBot"],  # for some chat stuff
     version="0.0.0",
